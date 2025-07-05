ab62f13c3dd0c8221312fb5cf8bed101606e6a02
ojara <ojarafavour@yahoo.com>
2025-07-05 15:24:26 +0100

    added project 8

--- alx-project-0x08/interfaces/index.ts
+++ alx-project-0x08/interfaces/index.ts
@@ -3,3 +3,12 @@ import * as React from "react"
 export interface ReactComponentProps {
     children: React.ReactNode
 }
+export interface GeneratedImageProps {
+  imageUrl: string
+  prompt: string
+  width?: string
+  height?: string
+  action: (imagePath: string) => void
+}
+
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
